124a3ebc68f2375b800b8e751ede6c2857b919d1
ELDO-TECH <eldorado.design1@gmail.com>
2026-08-29 14:03:51 +0100

    fix: refresh PWA cache after deployment changes

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE = "spsa-cobil-shell-v6";
+const CACHE = "spsa-cobil-shell-v7";
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(["./", "./manifest.webmanifest", "./icons/spsa-cobil-logo.png"])).then(() => self.skipWaiting())));
-self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
   event.respondWith(fetch(event.request).then((response) => { const copy = response.clone(); caches.open(CACHE).then((cache) => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request).then((cached) => cached || caches.match("./"))));
